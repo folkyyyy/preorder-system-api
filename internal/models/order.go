4ccb126c34197c87d8b81c8082ebc29ddb8b5933
jedtapungkee <jedtapungkee.c@ku.th>
2026-03-26 14:57:25 +0700

    fix: add json in model

--- internal/models/order.go
+++ internal/models/order.go
@@ -5,24 +5,24 @@ import "gorm.io/gorm"
 // ข้อมูลหลักของใบออเดอร์ลูกค้า
 type Order struct {
 	gorm.Model
-	UserID           uint          `gorm:"not null"`
-	Name			 string        `gorm:"not null"` // ชื่อผู้รับ
-	PreorderRoundID  uint          `gorm:"not null"` // ออเดอร์นี้สั่งในรอบไหน
+	UserID           uint          `gorm:"not null" json:"userId"`          // ใครเป็นคนสั่ง
+	Name             string        `gorm:"not null" json:"name"`            // ชื่อผู้รับ
+	PreorderRoundID  uint          `gorm:"not null" json:"preorderRoundId"` // ออเดอร์นี้สั่งในรอบไหน
 	PreorderRound    PreorderRound `gorm:"foreignKey:PreorderRoundID"`
-	DeliveryLocation string
-	TotalAmount      float64
-	Status           string        `gorm:"default:'pending'"` // pending, paid, completed
-	
+	DeliveryLocation string        `json:"deliveryLocation"`
+	TotalAmount      float64       `json:"totalAmount"`
+	Status           string        `gorm:"default:'pending'" json:"status"` // pending, paid, completed
+
 	OrderItems []OrderItem `gorm:"foreignKey:OrderID"`
 }
 
 // รายการอาหารที่อยู่ในใบออเดอร์
 type OrderItem struct {
 	gorm.Model
-	OrderID        uint         `gorm:"not null"`
-	PreorderMenuID uint         `gorm:"not null"` // ชี้ไปที่เมนูของรอบนั้นๆ
+	OrderID        uint         `gorm:"not null" json:"orderId"`        // ชี้ไปที่ใบออเดอร์หลัก
+	PreorderMenuID uint         `gorm:"not null" json:"preorderMenuId"` // ชี้ไปที่เมนูของรอบนั้นๆ
 	PreorderMenu   PreorderMenu `gorm:"foreignKey:PreorderMenuID"`
-	Quantity       int          `gorm:"not null"`
-	Note           string       // หมายเหตุ เช่น ไม่ใส่กระเทียม
-	PriceAtOrder   float64      // เก็บราคา ณ ตอนที่สั่ง (เผื่ออนาคตเมนูหลักขึ้นราคา บิลเก่าจะได้ไม่เพี้ยน)
+	Quantity       int          `gorm:"not null" json:"quantity"`
+	Note           string       `json:"note"`         // หมายเหตุ เช่น ไม่ใส่กระเทียม
+	PriceAtOrder   float64      `json:"priceAtOrder"` // เก็บราคา ณ ตอนที่สั่ง (เผื่ออนาคตเมนูหลักขึ้นราคา บิลเก่าจะได้ไม่เพี้ยน)
 }
--- internal/models/user.go
+++ internal/models/user.go
@@ -4,8 +4,8 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model        // ใส่บรรทัดนี้ GORM จะสร้างฟิลด์ ID, CreatedAt, UpdatedAt, DeletedAt ให้เราอัตโนมัติ
-	Email      string `gorm:"uniqueIndex;not null"`
-	Username   string
-	Password   string `gorm:"not null"`
-	Role       string `gorm:"default:'user'"` // เอาไว้แยก 'admin' หรือ 'user'
+	Email      string `gorm:"uniqueIndex;not null" json:"email"` // ต้องไม่ซ้ำกัน และห้ามเป็นค่าว่าง
+	Username   string `json:"username"`                          // ต้องไม่ซ้ำกัน และห้ามเป็นค่าว่าง
+	Password   string `gorm:"not null" json:"password"`          // ห้ามเป็นค่าว่าง
+	Role       string `gorm:"default:'user'" json:"role"`        // กำหนดค่าเริ่มต้นเป็น "user"
 }
