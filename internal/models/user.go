94051bc16f222aa7f4f7e0c733610d36ea23e71f
folkyyyy <jedta02@gmail.com>
2026-03-28 15:21:04 +0700

    fix: add order into model user and preorder_round

--- internal/models/user.go
+++ internal/models/user.go
@@ -12,9 +12,10 @@ const (
 )
 
 type User struct {
-	gorm.Model        // ใส่บรรทัดนี้ GORM จะสร้างฟิลด์ ID, CreatedAt, UpdatedAt, DeletedAt ให้เราอัตโนมัติ
-	Email      string `gorm:"uniqueIndex;not null" json:"email"` // ต้องไม่ซ้ำกัน และห้ามเป็นค่าว่าง
-	Username   string `json:"username"`
-	Password   string `gorm:"not null" json:"password"` // ห้ามเป็นค่าว่าง
-	Role       Role   `gorm:"type:varchar(20); default:'user'" json:"role"`
+	gorm.Model         // ใส่บรรทัดนี้ GORM จะสร้างฟิลด์ ID, CreatedAt, UpdatedAt, DeletedAt ให้เราอัตโนมัติ
+	Email      string  `gorm:"uniqueIndex;not null" json:"email"` // ต้องไม่ซ้ำกัน และห้ามเป็นค่าว่าง
+	Username   string  `json:"username"`
+	Password   string  `gorm:"not null" json:"password"` // ห้ามเป็นค่าว่าง
+	Role       Role    `gorm:"type:varchar(20); default:'user'" json:"role"`
+	Orders     []Order `gorm:"foreignKey:UserID"`
 }
