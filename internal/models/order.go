6b3ddd2fdab6eb25731a42a470787e8113437544
Jedtapungkee <151804548+Jedtapungkee@users.noreply.github.com>
2026-03-31 19:05:57 +0700

    fix: model order change userId can be null

--- internal/models/order.go
+++ internal/models/order.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 // ข้อมูลหลักของใบออเดอร์ลูกค้า
 type Order struct {
 	gorm.Model
-	UserID           uint          `gorm:"not null" json:"userId"`          // ใครเป็นคนสั่ง
+	UserID           *uint          `json:"userId"`          // ใครเป็นคนสั่ง
 	Name             string        `gorm:"not null" json:"name"`            // ชื่อผู้รับ
 	PreorderRoundID  uint          `gorm:"not null" json:"preorderRoundId"` // ออเดอร์นี้สั่งในรอบไหน
 	PreorderRound    PreorderRound `gorm:"foreignKey:PreorderRoundID"`
